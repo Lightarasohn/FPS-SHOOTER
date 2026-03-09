ae9037471c71644bf2be90ab2a1f735cd104ad30
Onur Demir <onur.demir.developer@outlook.com>
2026-03-10 02:47:04 +0300

    Development notes for player movement
    Kontroller hakkında benim düşündüğüm geliştirmeler ve nasıl yapılacağını anlatmak için bazı notlar.
    * Photon Fusion dökümantasyonu -> "Tutorials" -> "Host" -> "setting up a scene" ve "prediction" bölümlerine bakın

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -3,5 +3,17 @@ using UnityEngine;
 
 public class PlayerMovement : NetworkBehaviour
 {
-
+    // PlayerPrefab İÇİNDEKİ "NETWORK TRANSFORM" KULLANILACAK, SERVER İLE HABERLEŞEBİLMESİ İÇİN.
+    /*
+     * Yürüyüşe başlama keskin
+     * Yürüyüş sonunda ters yöne basılmamışsa hızlıca yavaşlayarak durma
+     * Yürüyüş yapılırken ters yöne basılmışsa 
+       -AYNI ANDA İKİ YÖNE DE BASILABİLİR, BİR YÖNE GİDERKEN GİTMEYİ BIRAKIP TERS YÖNE DE BASILABİLİR-
+       anında durma (Counter Strafing)
+     * Shift tuşuna koşma ya da yavaş yürüme özelliği (kendin karar ver)
+     * Zıplama
+     * Yer ve Hava süratleri farklı (sv_accelerate ve sv_airaccelerate)
+     * Air strafe
+     * CTRL = EĞİLME (???)
+    */
 }
