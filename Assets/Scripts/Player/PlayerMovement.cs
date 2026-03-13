0d2095d783c2cb863f69f6c542a40deda9e9e2b4
Bengi <114860608+Egemen-Bengi@users.noreply.github.com>
2026-03-13 15:20:05 +0300

    PlayerMovement scripti temel hareketler ileri geri sağa ve sola gitme yapıldı (devam etcem)

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,5 +1,6 @@
 using Fusion;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : NetworkBehaviour
 {
@@ -18,4 +19,54 @@ public class PlayerMovement : NetworkBehaviour
      * CTRL = EĞİLME 
      * akla geldikçe eklenebilir.
     */
+
+    [Header("Movement")]
+    public float moveSpeed = 6f;
+
+    public Transform orientation;
+
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 moveDirection;
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    private void Update()
+    {
+        //if (!HasInputAuthority) return; Karakter spawn edilirken açılcak
+
+        MyInput();
+    }
+
+    private void FixedUpdate()
+    {
+        //if (!HasInputAuthority) return;
+
+        MovePlayer();
+    }
+
+    private void MyInput()
+    {
+        Vector2 input = Keyboard.current != null ?
+            new Vector2(
+                (Keyboard.current.dKey.isPressed ? 1 : 0) - (Keyboard.current.aKey.isPressed ? 1 : 0),
+                (Keyboard.current.wKey.isPressed ? 1 : 0) - (Keyboard.current.sKey.isPressed ? 1 : 0)
+            ) : Vector2.zero;
+
+        horizontalInput = input.x;
+        verticalInput = input.y;
+    }
+
+    private void MovePlayer()
+    {
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+    }
 }
