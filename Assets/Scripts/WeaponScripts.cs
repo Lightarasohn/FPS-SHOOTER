afd76c0993c8194bf402e91fb5bd3428c37bf84e
Volkan Çelebi <volkan_celebii@hotmail.com>
2026-03-26 11:03:40 +0300

    aim gizmos
    + aim gizmos eklendi
    - ateş edildiğinde gizmos rengi değişmiyor, düzenlenmesi lazım

--- Assets/Scripts/WeaponScripts.cs
+++ Assets/Scripts/WeaponScripts.cs
@@ -5,9 +5,11 @@ public class Weapon : NetworkBehaviour
 {
     public Transform firePoint;
     public float range = 100f;
+    private bool isShooting = false;
 
     public void Shoot(NetworkRunner runner, PlayerRef player)
     {
+        isShooting = true;
         if (runner.LagCompensation.Raycast(
             firePoint.position,
             firePoint.forward,
@@ -23,6 +25,20 @@ public class Weapon : NetworkBehaviour
             {
                 health.TakeDamage(10);
             }
+        }
+        isShooting = false;
+    }
+
+    public void OnDrawGizmos()
+    {
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(firePoint.position, firePoint.position + (firePoint.forward * range));
+
+        if (isShooting)
+        {
+            Debug.Log("Shooting Gizmos");
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawLine(firePoint.position, firePoint.position + (firePoint.forward * range));
         }
     }
 }
