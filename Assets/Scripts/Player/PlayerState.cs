cd7276c16d958ae02ddeef22d70df164bb30f9d5
Onur Demir <onur.demir.developer@outlook.com>
2026-05-13 15:08:30 +0300

    Revert "Team ReSelect (GameState not working properly)"
    This reverts commit 3ba52efa660ddab0b9b30c058292fe60105d677d.

--- Assets/Scripts/Player/PlayerState.cs
+++ Assets/Scripts/Player/PlayerState.cs
@@ -5,7 +5,7 @@ using static GlobalVariables;
 public class PlayerState : NetworkBehaviour
 {
     public static PlayerState Local; // Arayüzün bu objeyi bulması için referans
-    public static bool IsSpawned = false;
+
 
     [Header("Spawn Settings")]
     [SerializeField] private NetworkPrefabRef _characterPrefab; // Oynayacağın asıl karakter prefabı
@@ -56,59 +56,8 @@ public class PlayerState : NetworkBehaviour
 
             // Doğmuş olan karakteri sunucu hafızasına kaydet
             Runner.SetPlayerObject(Object.InputAuthority, character);
-            IsSpawned = true;
 
             Debug.Log($"[PlayerState] Oyuncu {Object.InputAuthority.RawEncoded} {team} takımında, {spawnPos} konumunda doğdu.");
-        }
-    }
-
-    [Rpc(RpcSources.InputAuthority, RpcTargets.StateAuthority)]
-    public void RPC_RequestTeamChange(Team newTeam)
-    {
-        if (Object.HasStateAuthority)
-        {
-            // Runner üzerinden bu oyuncunun (InputAuthority) asıl karakter objesini buluyoruz.
-            // RPC_RequestSpawn içinde Runner.SetPlayerObject ile bunu kaydetmiştik.
-            if (Runner.TryGetPlayerObject(Object.InputAuthority, out NetworkObject characterObj))
-            {
-                Player physicalPlayerScript = characterObj.GetComponent<Player>();
-                if (physicalPlayerScript != null)
-                {
-                    // Eğer oyuncu zaten seçtiği takımdaysa hiçbir şey yapma
-                    if (physicalPlayerScript.PlayerTeam == newTeam) return;
-
-                    // 1. Takımı Güncelle
-                    physicalPlayerScript.PlayerTeam = newTeam;
-
-                    // 2. Yeni takımın başlangıç noktasına anında ışınla
-                    if (GameManager.Instance != null)
-                    {
-                        Transform teamSpawn = GameManager.Instance.GetSpawnPointForTeam(newTeam);
-                        if (teamSpawn != null)
-                        {
-                            physicalPlayerScript.transform.position = teamSpawn.position;
-                            physicalPlayerScript.transform.rotation = teamSpawn.rotation;
-                        }
-
-                        // İSTEĞE BAĞLI: Takım değiştirdiği için canını ve zırhını sıfırla (fulle)
-                        physicalPlayerScript.Health = 100;
-                        physicalPlayerScript.Armor = 100;
-                        physicalPlayerScript.ClearDamageHistory();
-
-                        // Silah mermilerini sıfırla
-                        if (physicalPlayerScript.EquippedWeapon != null)
-                        {
-                            physicalPlayerScript.EquippedWeapon.ResetAmmo();
-                        }
-
-                        // 3. Takım dengeleri değiştiği için (eski takımda kimse kalmamış olabilir)
-                        // kazanma durumunu tekrar kontrol ettir.
-                        GameManager.Instance.CheckWinCondition();
-                    }
-
-                    Debug.Log($"[PlayerState] Oyuncu {Object.InputAuthority.RawEncoded} {newTeam} takımına geçti!");
-                }
-            }
         }
     }
 }
