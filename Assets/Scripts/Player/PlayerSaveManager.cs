389463ea59008b71b1d26c4f0d35c61545f31f51
Onur Demir <114931736+Lightarasohn@users.noreply.github.com>
2026-05-16 22:43:15 +0300

    Mouse Settings

--- Assets/Scripts/Player/PlayerSaveManager.cs
+++ Assets/Scripts/Player/PlayerSaveManager.cs
@@ -4,7 +4,27 @@ using static GlobalVariables;
 public static class PlayerSaveManager
 {
     // Veriyi PlayerPrefs içinde hangi isimle tutacağımızı belirliyoruz
-    private const string SAVE_KEY = "PlayerCrosshairSettings";
+    private const string SAVE_KEY_CROSSHAIR = "PlayerCrosshairSettings";
+    private const string SAVE_KEY_MOUSE = "PlayerMouseSettings";
+
+    public static void SaveMouseSettings(MouseSettings mouseSettings)
+    {
+        string mouseSettingsJson = JsonUtility.ToJson(mouseSettings);
+        PlayerPrefs.SetString(SAVE_KEY_MOUSE, mouseSettingsJson);
+        PlayerPrefs.Save();
+    }
+
+    public static MouseSettings LoadMouseSettings()
+    {
+        if (PlayerPrefs.HasKey(SAVE_KEY_MOUSE))
+        {
+            string mouseSettingsJson = PlayerPrefs.GetString(SAVE_KEY_MOUSE);
+            MouseSettings loadedMouseSettings = JsonUtility.FromJson<MouseSettings>(mouseSettingsJson);
+            return loadedMouseSettings;
+        }
+        // Eğer ilk defa oyuna giriliyorsa (kayıt yoksa) varsayılanları döndür
+        return new MouseSettings(1, false, 25f, false, 2f, 0.5f, 3f);
+    }
 
     // Ayarları Kaydetme Fonksiyonu
     public static void SaveCrosshair(Crosshair data)
@@ -13,7 +33,7 @@ public static class PlayerSaveManager
         string jsonString = JsonUtility.ToJson(data);
 
         // Metni cihaza kaydet
-        PlayerPrefs.SetString(SAVE_KEY, jsonString);
+        PlayerPrefs.SetString(SAVE_KEY_CROSSHAIR, jsonString);
         PlayerPrefs.Save();
     }
 
@@ -21,9 +41,9 @@ public static class PlayerSaveManager
     public static Crosshair LoadCrosshair()
     {
         // Eğer daha önce kaydedilmiş bir ayar varsa onu oku
-        if (PlayerPrefs.HasKey(SAVE_KEY))
+        if (PlayerPrefs.HasKey(SAVE_KEY_CROSSHAIR))
         {
-            string jsonString = PlayerPrefs.GetString(SAVE_KEY);
+            string jsonString = PlayerPrefs.GetString(SAVE_KEY_CROSSHAIR);
             return JsonUtility.FromJson<Crosshair>(jsonString);
         }
 
